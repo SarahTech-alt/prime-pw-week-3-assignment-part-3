815e9549cb749c16c68be4ad911bac41aa68f429
Sarah Fuoss <sfuoss@gmail.com>
2021-07-29 11:14:15 -0500

    finished question five

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -43,36 +43,46 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
-for (let i=0; i<supplyChanges.max;i++){
-do {
-  console.log('added x parts.');
-} while (supplyChanges[i]>0);
-i++
-}
-/*
-for (let i = 0; i<supplyChanges.max; i++){
-  while (supplyChanges[i]>0){
-    console.log('added x parts');
-    i++;
+
+for (i=0; i<supplyChanges.length; i++){
+  if (supplyChanges[i]>0){
+    console.log(`added ${supplyChanges[i]} parts`);
   }
-  while (supplyChanges[i] === 0){
+  else if (supplyChanges[i] === 0){
     console.log('No Change.');
-    i++;
   }
-  while (supplyChanges[i]<1){
-    console.log('Removed x parts');
-    i++;
+  else if (supplyChanges[i]<1){
+    console.log(`Removed ${supplyChanges[i]} parts`);
   }
 }
-*/
+
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
+
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
+
+i = 0;
+
+
+while (supplyChanges[i]){
+  if (supplyChanges[i] > 0){
+    console.log(`added ${supplyChanges[i]} parts`);
+    i++;
+  }
+  else if (supplyChanges[i] === 0){
+    console.log('No Change.');
+    i++;
+  }
+  else {
+    console.log(`Removed ${supplyChanges[i]} parts`);
+    i++;
+  }
+  }
 
 
 // 9. Write a loop to determine the total number of parts available by
